596a076b362e729c0b2e94b15492958f7fbd60d6
HAKANKOKCU <103432992+HAKANKOKCU@users.noreply.github.com>
2022-07-27 17:02:04 +0300

    Fix crash at first startup

--- Budgie Dock/LogSaver.vb
+++ Budgie Dock/LogSaver.vb
@@ -1,5 +1,6 @@
 ﻿Module LogSaver
     Sub insertToLog(ByVal text As String)
+        On Error Resume Next
         My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\BudgieDock\log.log", vbNewLine & Now.Day & "/" & Now.Month & "/" & Now.Year & " (D/M/Y), " & Now.Second & ":" & Now.Minute & ":" & Now.Hour & " = " & text, True)
     End Sub
 End Module
